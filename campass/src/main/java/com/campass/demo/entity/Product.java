02526dbe98cb3262968cc67b7170d3c826efda5d
bona <leebona0301@gmail.com>
2022-10-06 15:32:44 +0900

    1006 뽀

--- campass/src/main/java/com/campass/demo/entity/Product.java
+++ campass/src/main/java/com/campass/demo/entity/Product.java
@@ -11,12 +11,9 @@ public class Product {
 	private Integer pCode;
 	private String pName;
 	private String pdtImg;
-	private String pdtDeImg;
-	private String pContent;
+	private String pContent;	// 상세이미지
 	private String pBrand;
 	private Integer pStarAvg;
 	private Integer pPrice;
-	private String pCateCode;
-	private String pCateName;
 }
  
